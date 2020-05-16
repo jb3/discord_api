1498394a068ee6f35da1dd1b35d0766a5eb4063f
Joseph Banks <joseph@josephbanks.me>
2020-05-16 20:54:38 +0100

    move library files to lib folder

---
+++ lib/http.rs
@@ -0,0 +1,45 @@
+use reqwest::{Error, Client};
+
+use crate::models::Invite;
+
+const API_BASE: &str = "https://discord.com/api/v7";
+
+/// Constructs a new client with a User-Agent compliant with the Discord API reference
+fn create_client() -> Client {
+    debug!("Creating new HTTP client");
+    Client::builder()
+        .user_agent("DiscordBot (https://github.com/jos-b/discord, 1.0)")
+        .build().expect("Could not build HTTP client")
+}
+
+/// Builds a route using the constant API_BASE to the specified path
+fn build_route(path: &str) -> String {
+    debug!("Building route to: {}", path);
+    format!("{}{}", API_BASE, path)
+}
+
+/// Fetches an invite from the Discord API.
+///
+/// Examples
+///
+/// ```
+/// # #[tokio::main]
+/// # async fn main() {
+/// use discord_api::get_invite;
+///
+/// let guild_invite = get_invite("python").await.expect("Could not get invite, request error");
+///
+/// println!("Guild invite for: {}", guild_invite.guild.expect("No guild found!").name);
+/// # }
+/// ```
+pub async fn get_invite<T: std::fmt::Display>(code: T) -> Result<Invite, Error> {
+    info!("Starting request to get invite: {}", code);
+    let invite: Invite = create_client()
+        .get(&build_route(&format!("/invites/{}", code)))
+        .send()
+        .await?
+        .json()
+        .await?;
+
+    Ok(invite)
+}
---
+++ lib/lib.rs
@@ -0,0 +1,33 @@
+//! A library for fetching various structures from the Discord API. This is not a fully fledged Discord wrapper
+//! nor does it attempt to be. It contains some structures deemed useful.
+//!
+//! This library should maintain feature parity with the included binary, available at https://github.com/jos-b/discord
+//! in that all new structures added to this library should be accessible in some way through the binary.
+//!
+//! # Examples
+//!
+//! Fetch a guild invite and list the features
+//!
+//! ```
+//! # #[tokio::main]
+//! # async fn main() {
+//! use discord_api::get_invite;
+//!
+//! let invite = get_invite("python").await.unwrap();
+//!
+//! if let Some(guild) = invite.guild {
+//!   for feature in guild.features {
+//!     println!("{}", feature);
+//!   }
+//! }
+//! # }
+//! ```
+
+
+#[macro_use]
+extern crate log;
+
+pub mod http;
+pub mod models;
+
+pub use http::get_invite;
